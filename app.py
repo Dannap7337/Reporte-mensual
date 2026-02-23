2d32577031fe974e0767249e8b67104139ba9642
Danna Patricia Riveroll <dannap7337@gmail.com>
2026-02-23 16:04:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ LINKS_TIMELINE = {
     (2025, 9): "https://lucid.app/lucidspark/b6d966fe-81c8-4c80-b434-8b887b9f478c/edit?invitationId=inv_0789d6c9-c78c-43fa-b137-445bee6dd70c",
     (2025, 10): "https://lucid.app/lucidspark/fa0b5127-cb34-48b6-ab4d-760d38ac95d5/edit?invitationId=inv_f9d4919f-3afb-4862-8abd-a3fa7e90c52a",
     (2025, 11): "https://lucid.app/lucidspark/487992bf-7d7d-4eab-a389-6ccfae58c557/edit?invitationId=inv_25f6128c-a3ec-4f65-a58f-21be6ac896c6",
-    (2025, 12): "https://lucid.app/lucidspark/fd3b8c79-5408-495f-b2ac-f1a58b043db7/edit?invitationId=inv_54a83472-e357-462a-9493-7172fe0b7757",
-    (2026, 1): "https://tu-enlace-enero-2026.com",
-    (2026, 2): "https://tu-enlace-febrero-2026.com"
+    (2025, 12): "https://lucid.app/lucidspark/fd3b8c79-5408-495f-b2ac-f1a58b043db7/edit?invitationId=inv_54a83472-e357-462a-9493-7172fe0b7757"
 }
 
 # --- CSS ---
@@ -446,4 +444,5 @@ if df is not None:
 
 else:
     st.error("No se encontró 'Tickets año.xlsx'")
+
 
