9a9db4eda45f0ecc27e6f0622b36b2b1653baa75
Danna Patricia Riveroll <dannap7337@gmail.com>
2026-04-08 12:00:51 -0600

    Restore original style and functionality in app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def calcular_estatus_solucion(row, fecha_limite, fecha_inicio_mes):
 
 def calcular_detalle_solucion(row):
     if row['Estatus_Solucion'] == 'Fuera':
-        gen_val = str(row['Generación_Excel']).strip().lower()
+        gen_val = str(row['Generacion_Excel']).strip().lower()
         if 'mismo' in gen_val:
             return 'Asap'
         txt = str(row['RANGO']).lower()
